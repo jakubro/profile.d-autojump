955532eb37d2482968fe20c2f654503312118279
Jakub Roman <jakub@jakubroman.sk>
2023-11-27 14:52:55 +0100

    reduce git verbosity

--- bin/install.sh
+++ bin/install.sh
@@ -5,7 +5,7 @@ log_info "Installing/Updating autojump..."
 dir=$(mktemp -d /tmp/profile.d.XXXXXXXXXX) || exit 1
 
 cd "$dir" &&
-  git clone https://github.com/wting/autojump . &&
+  git clone --quiet https://github.com/wting/autojump . &&
   SHELL=bash ./install.py -d ~/.autojump &&
   log_info "You can safely ignore any instructions printed above"
 
