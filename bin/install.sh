c3afa0bc8db142eced238084bea4f350cfc0b9fe
Jakub Roman <jakub@jakubroman.sk>
2024-01-20 15:35:41 +0100

    linting

--- bin/install.sh
+++ bin/install.sh
@@ -2,16 +2,16 @@
 
 log_info "Installing/Updating autojump..."
 
-dir=$(mktemp -d /tmp/profile.d.XXXXXXXXXX) || exit 1
+temp_dir=$(mktemp -d /tmp/profile.d.XXXXXXXXXX) || exit 1
 
-cd "$dir" &&
-  git clone --quiet https://github.com/wting/autojump . &&
-  SHELL=bash ./install.py -d ~/.autojump &&
-  log_info "You can safely ignore any instructions printed above"
+cd "$temp_dir" \
+  && git clone --quiet https://github.com/wting/autojump . \
+  && SHELL=bash ./install.py -d ~/.autojump \
+  && log_info "You can safely ignore any instructions printed above"
 
 code=$?
 
-rm -rf "$dir" || exit 1
+rm -rf "$temp_dir" || exit 1
 
 if [ "$code" != 0 ]; then
   exit 1
